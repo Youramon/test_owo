052e8ecbceb5a17998403f535c1f290a484d9c92
youramon <bay000r.man@gmail.com>
2022-12-04 18:58:16 +0100

    ohne cloud firestore

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:test_owo/andere/lists.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+// import 'package:firebase_core/firebase_core.dart';
+import 'package:test_owo/firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,29 +21,40 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      darkTheme: ThemeData(brightness: Brightness.dark),
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: Scaffold(
-        body: screens[_currentIndex],
-        bottomNavigationBar: CurvedNavigationBar(
-            backgroundColor: Colors.transparent,
-            animationDuration: const Duration(milliseconds: 300),
-            //  animationCurve: Curves.bounceInOut,
-            color: const Color.fromARGB(255, 68, 66, 66),
-            items: const [
-              // ignore: prefer_const_constructors
-              Icon(Icons.question_mark, size: 30, color: Colors.blueAccent),
-              Icon(MdiIcons.pokeball, size: 30, color: Colors.blueAccent),
-            ],
-            onTap: (index) {
-              setState(() {
-                _currentIndex = index;
-              });
-            }),
-      ),
-      debugShowCheckedModeBanner: false,
-    );
+        darkTheme: ThemeData(brightness: Brightness.dark),
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+        ),
+        /* home: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done: */
+        home: Scaffold(
+            body: screens[_currentIndex],
+            bottomNavigationBar: CurvedNavigationBar(
+                backgroundColor: Colors.transparent,
+                animationDuration: const Duration(milliseconds: 300),
+                //  animationCurve: Curves.bounceInOut,
+                color: const Color.fromARGB(255, 68, 66, 66),
+                items: const [
+                  Icon(Icons.question_mark, size: 30, color: Colors.blueAccent),
+                  Icon(MdiIcons.pokeball, size: 30, color: Colors.blueAccent),
+                ],
+                onTap: (index) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                })));
+
+    //   default:
+    //   return const Center(
+    //     child: SizedBox(
+    // height: 50,
+    // width: 50,
+    // child: CircularProgressIndicator.adaptive(),
+    // ));
   }
 }
